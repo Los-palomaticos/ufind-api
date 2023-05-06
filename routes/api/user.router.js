81def35efcc3015609018df1b4911cfe3364d32b
Chompi <00004321@uca.edu.sv>
2023-05-06 09:54:28 -0600

    LoginV1

--- routes/api/user.router.js
+++ routes/api/user.router.js
@@ -1,9 +1,14 @@
 var express = require('express');
+const UserValidations = require('../../validators/user.validator');
+const runValidations = require('../../validators/index.validator');
 var router = express.Router();
 const userController = require('../../controllers/user.controller')
 /* GET users listing. */
 router.get('/', userController.getUser)
-router.post('/login', userController.login)
+router.post('/login', 
+UserValidations.login,
+runValidations,
+userController.login)
 router.post('/signup', userController.signup)
 
 module.exports = router;
