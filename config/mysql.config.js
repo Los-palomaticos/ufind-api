a655760125eeb4a440264546d6457bc8bda82719
Pyload901 <00060021@uca.edu.sv>
2023-05-15 22:38:06 -0600

    fix actualizar usuario, actualiza campos necesarios y valida los datos enviados por la peticion

--- config/mysql.config.js
+++ config/mysql.config.js
@@ -5,7 +5,7 @@ const sequelize = new Sequelize(
     process.env.DB_PASSWORD || '',
 {
     host: process.env.DB_HOST || 'localhost',
-    dialect: 'mariadb',
+    dialect: 'mysql',
     logging: false
 });
 const connect = async () => {
