be8097d6c851c6e276bd565e7c831b735c7660a7
Pyload901 <00060021@uca.edu.sv>
2023-06-11 22:47:56 -0600

    add documentacion para usuario

--- routes/api.js
+++ routes/api.js
@@ -4,6 +4,15 @@ const userRouter = require('./api/user.router')
 const postRouter = require('./api/post.router')
 const walletRouter = require('./api/wallet.router')
 
+// Swagger config
+const swaggerUi = require('swagger-ui-express');
+const fs = require("fs")
+const YAML = require('yaml')
+
+const file  = fs.readFileSync('./swagger/swagger.yaml', 'utf8')
+const swaggerDocument = YAML.parse(file)
+
+router.use('/', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 router.use('/user', userRouter)
 router.use('/post', postRouter)
 router.use('/wallet', walletRouter)
